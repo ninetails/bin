05d8a9f214c48b642d4c08334d1939a289d20ed6
Carlos Kazuo <ninetails.nine@gmail.com>
2018-04-14 01:24:27 -0300

    Added create Github repository

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
-console.log('Hello!') // eslint-disable-line no-console
+const { prompt } = require('inquirer')
+
+const questions = [
+  {
+    name: 'script',
+    message: 'What script to run?',
+    type: 'list',
+    choices: [
+      {
+        name: 'Create a github repository',
+        value: 'create-github-repo'
+      }
+    ]
+  }
+];
+
+(async () => {
+  const { script } = await prompt(questions)
+  require(`./scripts/${script}`)()
+})()
---
+++ scripts/create-github-repo.js
@@ -0,0 +1,28 @@
+const { spawn } = require('child_process')
+const { prompt } = require('inquirer')
+
+const url = 'https://api.github.com/user/repos'
+
+const questions = [
+  {
+    name: 'user',
+    message: 'Username',
+    type: 'input',
+    validate: input => !!input
+  },
+  {
+    name: 'repo',
+    message: 'Repository name',
+    type: 'input',
+    validate: input => !!input
+  }
+];
+
+module.exports = async () => {
+  const { user, repo } = await prompt(questions)
+  spawn('curl', ['-u', user, url, '-d', `{"name":"${repo}"}`], {
+    cwd: process.cwd(),
+    detached: true,
+    stdio: 'inherit'
+  })
+}
